f3636443f3b2e1919a65ee5e480946d0cdfc4376
Jason Stallings <jason@stallin.gs>
2015-04-10 01:09:40 -0500

    Complete rewrite for socket.io 1.0.

--- app.js
+++ app.js
@@ -21,60 +21,60 @@ function handler (req, res) {
   });
 }
 
-io.sockets.on('connection', function (socket) {
+io.on("connection", function(socket)
+{
 
-
-	socket.on('message', function (data)
-	{
-        console.log("message");
-		for (var room in io.nsps["/"].adapter.rooms[socket.id])
-		{
-            console.log()
-			if (room!="")
-			{
-
-				room=room.substring(1, room.length)
-				console.log(room);
-                io.to('some room').emit('receive', data);
-			}
-		}
-	})
-
-  socket.on('find', function (data) {
-
-	searching[socket.id]=socket
-
-	found="";
-    console.log("Socketid:" + socket.id);
-	for(var client in searching)
+    socket.on("find", function(msg)
     {
-		if (client!=socket.id)
-		{
-			found=searching[client]
-            console.log("found: " + found);
-			break
-		}
-
-		//socket.join(clients[i].id)
-
-	}
-	if (found!="")
-	{
-		 socket.emit('found', { friend: found.id});
-		 found.emit('found',  { friend: socket.id});
-		socket.join(socket.id)
-		found.join(socket.id)
-		 delete searching[socket.id];
-		 delete searching[found.id];
-
-	}
-	else
-	{
-		socket.emit('found', {friend: 'none'});
-	}
-
-  });
-
-
-
+        var found="";
+        
+        //Store client in searching list.
+        searching[socket.id]=socket;
+        
+	    for(var client in searching)
+        {
+		    if (client!=socket.id)
+		    {
+			    found=searching[client]
+                console.log("found: " + client);
+			    break
+		    }
+	   }
+       
+        if (found!="")
+	    {
+		    socket.emit("found", { friend: found.id});
+    		found.emit("found",  { friend: socket.id});
+            
+    		socket.join(socket.id);
+    		found.join(socket.id);
+            socket.join(found.id);
+    		found.join(found.id);
+            
+            delete searching[socket.id];
+            delete searching[found.id];
+        }
+    });
+    
+    
+    socket.on("chat message", function(msg)
+    {
+        socket.broadcast.to(socket.id).emit('receive', msg);
+        //console.log(io.sockets.adapter.rooms[socket.id]);
+        console.log('message: ' + msg);
+    });
+  
+  
 });
+
+function count()
+{
+    var i=0;
+    for (var client in searching)
+    {
+        i++;
+    }
+    return i;
+}
+
+
